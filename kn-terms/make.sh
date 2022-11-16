f8be2945aeb0c10b0a95fbe4fe5ff73f15141e94
Razumovskyy <mrazumovskyy@gmail.com>
2022-11-17 00:27:55 +0600

    stable version with good working tree, updated makefiles and README

--- kn-terms/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm ./Control/*
-find . -name '*.out' -type f -exec rm {} \;
-find . -name '*__?' -type f -exec rm {} \;
-rm CO2_COOLING_* CO2_Fdown_* CO2_Fup_*
- 
-gfortran PT_LW_main_TOT.f90 MOD_INI_PT_KD.f90 PT_Dflux_forKD.f90 PT_K_forKD.f90 PT_Planck_separate.f90 PT_Slon_AVT_SEPAR.f90 -o getnkd.out
-gfortran V-D_tau_SEP_Q.f90 V-fromTAUtoFLUX.f90 V-PLANCK_oper.f90 -o compare_tot.out
